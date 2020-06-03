c349f3f6587fad364909dcea1f0cc2fdac761d52
KULDEEP SINGH <kuldeep.singh@fisglobal.com>
2020-06-03 14:58:15 +0200

    settinng allhosts updated

--- conceptsite/conceptsite/settings.py
+++ conceptsite/conceptsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o2y!0i8ccv+)(e$&g#v@7fkuzw4(75kr_vh2(6&_u(t1)ya@m#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
