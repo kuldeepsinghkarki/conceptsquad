8cb4715ddd54cb43bfb415f96ba11bcada15fb3c
KULDEEP SINGH <kuldeep.singh@fisglobal.com>
2020-06-04 07:52:10 +0200

    added admin path

--- conceptsite/conceptsite/urls.py
+++ conceptsite/conceptsite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('payments/', include('PaymentsDemo.urls')),
 ]
