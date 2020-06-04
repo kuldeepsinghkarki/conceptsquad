a503d30fae45c2645c5ad41a4418a399450be547
KULDEEP SINGH <kuldeep.singh@fisglobal.com>
2020-06-04 09:07:17 +0200

    updated

--- conceptsite/conceptsite/urls.py
+++ conceptsite/conceptsite/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import include
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
@@ -27,7 +28,7 @@ urlpatterns = [
     path('payments/', include('PaymentsDemo.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
 
 
 
