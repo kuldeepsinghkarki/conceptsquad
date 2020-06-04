5348ba0f2f8f5e07ad5445e7ef7dc8631b5a3c01
KULDEEP SINGH <kuldeep.singh@fisglobal.com>
2020-06-04 12:41:16 +0200

    migrated to mysql

--- conceptsite/conceptsite/settings.py
+++ conceptsite/conceptsite/settings.py
@@ -77,11 +77,23 @@ WSGI_APPLICATION = 'conceptsite.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+  #  'user': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # },
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'paymentsdb',
+        'USER': 'fis@conceptsdbserver',
+        'PASSWORD': 'password@123',
+        'HOST': 'conceptsdbserver.mysql.database.azure.com',
+        'PORT': '3306',
     }
+  
 }
+
+
 
 
 # Password validation
